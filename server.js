d3efb63f8239945beacbaa16098027dce75a4a0c
JOE NYUGOH NGIGI <nyugoh@gmail.com>
2018-02-16 09:50:02 +0300

    :bug: Removed node engine directive

--- server.js
+++ server.js
@@ -16,13 +16,30 @@ server.use('/assets', express.static(require('path').join(__dirname, 'assets')))
 server.use('/docs', express.static(require('path').join(__dirname, 'docs')));
 server.use('/content', express.static(require('path').join(__dirname, 'admin/content')));
 server.use('/admin/assets', express.static(require('path').join(__dirname, 'admin/assets')));
-server.use('/api', proxy({target: 'http://35.224.196.185/', changeOrigin: true}));
-server.use('/socket.io', proxy({target: 'http://35.224.196.185/', changeOrigin: true}));
+// server.use('/api', proxy({target: 'http://0.0.0.0:9000/api/wpos.php', changeOrigin: true}));
+// server.use('/socket.io', proxy({target: 'http://35.224.196.185/', changeOrigin: true}));
+
+server.all('/api/:reqType', function(req, res, next){
+	if (req.body) {
+        let bodyData = JSON.stringify(req.body);
+        // incase if content-type is application/x-www-form-urlencoded -> we need to change to application/json
+        proxy.setHeader('Content-Type','application/json');
+        proxy.setHeader('Content-Length', Buffer.byteLength(bodyData));
+        // stream the content
+        proxy.write(bodyData);
+    }
+	var endPoint = 'http://0.0.0.0:9000/api/wpos.php?a='+req.params.reqType;
+	console.log(endPoint);
+  server.use('/api', proxy({target: endPoint, changeOrigin: true}));
+  	res.end();
+});
+
 
 // Routes
 server.get('/', function(req, res, next){
   res.sendFile(__dirname + '/index.html');
 });
+
 
 server.get('/wpos.appcache', (req, res, next)=>{
   res.sendFile(__dirname + '/wpos.appcache');
