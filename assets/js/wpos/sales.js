50c225c4f9d1a090dbc044364ef8e63dfbb2ff3e
KenyanGeek <chegeherman@gmail.com>
2018-02-16 14:01:13 +0300

    Improved UI of abort order

--- assets/js/wpos/sales.js
+++ assets/js/wpos/sales.js
@@ -833,11 +833,33 @@ function WPOSSales() {
     /**
      *
      */
+    this.test = function(){
+        swal(
+            'Good job!',
+            'You clicked the button!',
+            'success'
+          );
+    };
+
     this.userAbortSale = function () {
-        var answer = confirm("Are you sure you want to abort this order?");
+        /*var answer = confirm("Are you sure you want to abort this order?");
         if (answer) {
             clearSalesForm();
-        }
+        }*/
+// Implemented new modal for aborting orders
+        swal({
+            title: 'Are you sure you want to abort this order??',
+            type: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, abort it!'
+          }).then(function (result) {
+            if (result.value) {
+                clearSalesForm();
+              swal('Aborted!', 'Your order has been aborted.', 'success');
+            }
+          });
     };
 
     this.resetSalesForm = function(){
